16679e16487da5c8c6700960917ad9bb8d35d91f
Abdelaziz <abdelazizahmed917@gmail.com>
2022-07-27 03:31:36 +0200

    creating new class

---
+++ java_Learning_SE/src/main/java/introduction/VariablesandLiterals.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class VariablesandLiterals {
+    public static void main(String[] args) {
+        int n=10;
+        float nn=12.15f;
+        Double nnn=12.12515645;
+        System.out.println("the literals of n,nn,nnn"+n+"\n"+nn+"\n"+nnn);
+
+    }
+}
